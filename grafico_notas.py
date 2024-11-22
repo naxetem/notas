9ef161c7b2c2fdd4711b67af82627e653f2d0592
imcams <imcams@aemet.es>
2024-11-22 19:23:05 +0100

    nuevo gráfico. Frecuencias acumuladas

--- grafico_notas.py
+++ grafico_notas.py
@@ -2,6 +2,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Configurar la página en modo "wide"
+st.set_page_config(layout="wide")
+
 # Cargar los datos
 df_filtered = pd.read_csv("notas_disociadas.csv")
 
@@ -13,6 +16,9 @@ interval_counts = df_filtered['Intervalo'].value_counts().sort_index()
 
 # Convertir los intervalos a cadenas de texto
 interval_counts.index = interval_counts.index.astype(str)
+
+# Calcular la frecuencia acumulada
+interval_counts_acumulada = interval_counts.iloc[::-1].cumsum().iloc[::-1]
 
 # Interfaz de Streamlit
 st.title("Gráficos y Posición de Puntuaciones TAI 2023")
@@ -27,7 +33,7 @@ fig = px.bar(
     labels={'index': 'Puntuación mínima del intervalo', 'value': 'Frecuencia'},
     title="Distribución de Puntuaciones",
     color_discrete_sequence=["#636EFA"],  # Color profesional
-    text=interval_counts.values  # Mostrar valores en las barras
+    text=interval_counts_acumulada.values  # Mostrar valores en las barras
 )
 
 # Mejoras al diseño
@@ -42,6 +48,24 @@ fig.update_layout(
     plot_bgcolor="white",  # Fondo blanco
 )
 
+fig.update_traces(
+    text=interval_counts_acumulada.values,  # Mostrar valores acumulados en las barras
+    textposition='outside',  # Posición del texto fuera de las barras
+    textfont=dict(
+        size=30,  # Tamaño de la fuente del texto
+        color='black',  # Color de la fuente del texto
+        family='Arial'  # Familia de la fuente del texto
+    ),
+    marker=dict(
+        color='blue',  # Color de los marcadores
+        line=dict(
+            color='black',  # Color del borde de los marcadores
+            width=0  # Ancho del borde de los marcadores
+        )
+    ),
+    hovertext="Frecuencia acumulada",
+    opacity=0.8  # Opacidad de la traza
+)
 # Mostrar el gráfico en Streamlit
 st.plotly_chart(fig)
 
