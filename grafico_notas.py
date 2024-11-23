69c59d972b6507ffe99c716a5ac2f8f820c4df6e
Nacho <Naxetem@gmail.com>
2024-11-23 13:08:26 +0100

    Mejorada la visualización del gráfico de distribución de puntuaciones y se reorganizó el código para una mejor legibilidad.

--- grafico_notas.py
+++ grafico_notas.py
@@ -6,14 +6,19 @@ import plotly.graph_objects as go
 # Configurar la página en modo "wide"
 st.set_page_config(layout="wide")
 
+
+### procesado del dataset de notas ###
 # Cargar los datos
 df_filtered = pd.read_csv("notas_disociadas.csv")
 
+# Convertir los intervalos a cadenas que muestran solo el límite inferior
 min_value = int(48)
 max_value = int(140)
 
 df_filtered['Intervalo'] = pd.cut(df_filtered['Puntuacion total directa'], bins=range(min_value, max_value + 1, 1), right=False)
+
 interval_counts = df_filtered['Intervalo'].value_counts().sort_index()
+interval_counts.index = [str(interval.left) for interval in df_filtered['Intervalo'].cat.categories]
 
 # Convertir los intervalos a cadenas de texto
 interval_counts.index = interval_counts.index.astype(str)
@@ -21,56 +26,9 @@ interval_counts.index = interval_counts.index.astype(str)
 # Calcular la frecuencia acumulada
 interval_counts_acumulada = interval_counts.iloc[::-1].cumsum().iloc[::-1]
 
-# Interfaz de Streamlit
+
+### Interfaz de Streamlit ###
 st.title("Gráficos y Posición de Puntuaciones TAI 2023")
-
-# Convertir los intervalos a cadenas que muestran solo el límite inferior
-interval_counts.index = [str(interval.left) for interval in df_filtered['Intervalo'].cat.categories]
-
-# Gráfico de barras interactivo con etiquetas mejoradas
-#st.subheader("Distribución de puntuaciones")
-fig = px.bar(
-    interval_counts,
-    labels={'index': 'Puntuación mínima del intervalo', 'value': 'Frecuencia'},
-    title="Distribución de Puntuaciones",
-    color_discrete_sequence=["#636EFA"],  # Color profesional
-    text=interval_counts_acumulada.values  # Mostrar valores en las barras
-)
-
-# Mejoras al diseño
-fig.update_layout(
-    xaxis_title="Puntuación mínima del intervalo",
-    yaxis_title="Frecuencia",
-    title_font_size=20,
-    title_font_family="Arial",
-    title_x=0.5,
-    xaxis=dict(tickangle=0),  # Alineación horizontal
-    yaxis=dict(showgrid=True, gridcolor="lightgray"),  # Cuadrícula del eje Y
-    plot_bgcolor="white",  # Fondo blanco
-)
-
-fig.update_traces(
-    text=interval_counts_acumulada.values,  # Mostrar valores acumulados en las barras
-    textposition='outside',  # Posición del texto fuera de las barras
-    textfont=dict(
-        size=30,  # Tamaño de la fuente del texto
-        color='black',  # Color de la fuente del texto
-        family='Arial'  # Familia de la fuente del texto
-    ),
-    marker=dict(
-        color='blue',  # Color de los marcadores
-        line=dict(
-            color='black',  # Color del borde de los marcadores
-            width=0  # Ancho del borde de los marcadores
-        )
-    ),
-    hovertext="Frecuencia acumulada",
-    opacity=0.8  # Opacidad de la traza
-)
-# Mostrar el gráfico en Streamlit
-#Ocultado para evitar problemas de visualización
-#st.plotly_chart(fig)
-
 
 
 # Posición según puntuación
@@ -115,6 +73,46 @@ try:
 except Exception as e:
     st.write(f"Error al calcular la posición: {e}")
 
+#####Gráfico ######
+
+fig = px.bar(
+    interval_counts,
+    labels={'index': 'Puntuación mínima del intervalo', 'value': 'Frecuencia'},
+    title="Distribución de Puntuaciones",
+    color_discrete_sequence=["#636EFA"],  # Color profesional
+    text=interval_counts_acumulada.values  # Mostrar valores en las barras
+)
+
+# Mejoras al diseño
+fig.update_layout(
+    xaxis_title="Puntuación mínima del intervalo",
+    yaxis_title="Frecuencia",
+    title_font_size=20,
+    title_font_family="Arial",
+    title_x=0.5,
+    xaxis=dict(tickangle=0),  # Alineación horizontal
+    yaxis=dict(showgrid=True, gridcolor="lightgray"),  # Cuadrícula del eje Y
+    plot_bgcolor="white",  # Fondo blanco
+)
+
+fig.update_traces(
+    text=interval_counts_acumulada.values,  # Mostrar valores acumulados en las barras
+    textposition='outside',  # Posición del texto fuera de las barras
+    textfont=dict(
+        size=30,  # Tamaño de la fuente del texto
+        color='black',  # Color de la fuente del texto
+        family='Arial'  # Familia de la fuente del texto
+    ),
+    marker=dict(
+        color='blue',  # Color de los marcadores
+        line=dict(
+            color='black',  # Color del borde de los marcadores
+            width=0  # Ancho del borde de los marcadores
+        )
+    ),
+    hovertext="Frecuencia acumulada",
+    opacity=0.8  # Opacidad de la traza
+)
 # Añadir un marcador al gráfico para la nota total
 fig.add_trace(go.Scatter(x=[nota_total], y=[0], mode='markers+text', text=['Nota Total'], textposition='top center', marker=dict(color='red', size=10), name='Nota Total'))
 
